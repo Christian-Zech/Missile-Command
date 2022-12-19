2135788eece23837a7e0d597f86618486db65159
Owen Isenhart <isenhart.owen@gmail.com>
2022-12-19 11:48:45 -0600

    made basic explosion class and a list for them to be stored

---
+++ MissileCommand/MissileCommand/Explosion.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MissileCommand
+{
+    class Explosion
+    {
+        public Rectangle rect;
+        public Boolean done;
+        int temp;
+        public Explosion(Rectangle pos)
+        {
+            rect = pos;
+            done = false;
+            temp = 0;
+        }
+        public void Explode()
+        {
+            if (!done)
+            {
+                if (temp == 0)
+                {
+                    rect.Width++;
+                    rect.Height++;
+                    if (rect.Width > 30)
+                    {
+                        temp = 1;
+                    }
+                }
+                if (temp == 1)
+                {
+                    rect.Width--;
+                    rect.Height--;
+                    if (rect.Width < 5)
+                    {
+                        done = true;
+                    }
+                }
+            }
+        }
+    }
+}
